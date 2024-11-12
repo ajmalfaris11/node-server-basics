53cfb537b41a4d241324bab06c269db2ba354d72
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-12 17:56:16 +0530

    Start server on port 3200 with confirmation message in console

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+// Import the built-in HTTP module from Node.js
 const http = require('http')
 
+// Create an HTTP server
 const server = http.createServer(async (req, res) => {
+    
+    // Check if the request is for the root URL ("/") and is a GET request
     if(req.url === '/' && req.method === 'GET') {
-        res.write("Hello. i am here!")
-        res.end()
+        
+        // Respond with a message if the root URL is accessed
+        res.write("Hello. I am here!")
+        res.end()  // End the response
     }
     else {
+        // Respond with a message if the requested route is not defined
         res.end("no route present ")
     }
 })
 
+// Start the server and have it listen on port 3200
+server.listen(3200, () => {
+    // Log a message to the console confirming the server is running
+    console.log("Server running at port 3200")
+})
